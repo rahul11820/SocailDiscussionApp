5372dd9685aedd742c3aa48e02a51054e28d7c63
rk11820 <rahulajay.kumar@cogoport.com>
2024-07-08 00:10:50 +0530

    Elastic Config Changes

--- src/main/java/com/rahul/user/config/ElasticSearchConfig.java
+++ src/main/java/com/rahul/user/config/ElasticSearchConfig.java
@@ -11,42 +11,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.rahul.user.repository.elasticsearch")
 public class ElasticSearchConfig {
 
     @Bean
     public RestClient restClient() throws Exception {
-        TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-                    public void checkClientTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                    public void checkServerTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
 
-        SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, trustAllCerts, new java.security.SecureRandom());
-
-        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elastic", "7Q6wiCSWpTs1V9lZyE9C"));
-
-        RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "https"))
-                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
-                        .setDefaultCredentialsProvider(credentialsProvider)
-                        .setSSLContext(sc)
-                        .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE));
-
+        RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "http"))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder);
         return builder.build();
     }
 }
